f64b06414b6baffaf66998c909ae4aefb5bbf942
snehal_dave <snehal_dave@NY01-2KQNKV1.mhf.mhc>
2016-11-02 11:15:23 -0400

    added SOP

--- workflow2/src/workflow2/Confirm.java
+++ workflow2/src/workflow2/Confirm.java
@@ -4,6 +4,6 @@ public class Confirm {
 
 	public static void main(String args[]){
 		System.out.println("Joon Park");
-		System.out.println("Snehal Dave");
+		System.out.println("Snehal Dave"); 
 	}
 }
