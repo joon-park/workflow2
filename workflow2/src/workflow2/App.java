04ba29b7ff1498fb90bbfd43a1247e43e1423b99
joon_park <joon_park@NY01-8PS8CY1.mhf.mhc>
2016-11-03 16:58:24 -0400

    added int Joonnumber

--- workflow2/src/workflow2/App.java
+++ workflow2/src/workflow2/App.java
@@ -2,6 +2,8 @@ package workflow2;
 
 public class App {
 	private Object riteshsLock_;
+	private int joonnumber;
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
