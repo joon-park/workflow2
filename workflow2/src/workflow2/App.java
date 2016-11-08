9fac0fbb46505b8a2ed44f7a6536134d647c8e67
gopalkolluri <gopala.kolluri@spdji.com>
2016-11-08 21:31:17 +0000

    Update App.java

--- workflow2/src/workflow2/App.java
+++ workflow2/src/workflow2/App.java
@@ -6,11 +6,9 @@ public class App {
 
 		System.out.println("Joon's edit");
 		System.out.println("Hello, this is Ritesh.");
-
 		System.out.println("Tarak.");
-
-
 		System.out.println("Ritesh via GitHub.");
 		System.out.println("And Ritesh again.");
+		System.out.println("comments updated");
 	}
 }
